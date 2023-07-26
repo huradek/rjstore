c744d0413f37f5239c1264c0e623b08bb1736790
Rahul <rahuldeka297@gmail.com>
2023-07-26 17:41:00 +0530

    api update

--- client/src/components/Products.jsx
+++ client/src/components/Products.jsx
@@ -20,8 +20,8 @@ const Products = ({ cat, filters, sort }) => {
             try {
                 const res = await axios.get(
                     cat
-                        ? `http://localhost:5000/api/products?category=${cat}`
-                        : "http://localhost:5000/api/products"
+                        ? `http://rjstore.onrender.com/api/products?category=${cat}`
+                        : "http://rjstore.onrender.com/api/products"
                 );
                 setProducts(res.data);
             } catch (err) { }
