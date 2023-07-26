8da8c0aa870e171687320271221a55d7e7b1772f
Rahul <rahuldeka297@gmail.com>
2023-07-26 17:02:30 +0530

    api url

--- client/src/requestMethods.js
+++ client/src/requestMethods.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL = "http://localhost:5000/api/";
+const BASE_URL = "https://rjstore.onrender.com/api/";
 const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0YmM5ODkzZDk4YTg4MTg5ZjdiOWQ4NCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY5MDE5MDkwMSwiZXhwIjoxNjkwNDUwMTAxfQ.L3kHul895_tlOb_LyKzZDRUlOrvRS6wJK2Ee_fI5-wE";
 
 export const publicRequest = axios.create({
