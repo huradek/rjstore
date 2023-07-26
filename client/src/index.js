b875fec4562a388560b22ee9152f7bfaab4148d6
Rahul <rahuldeka297@gmail.com>
2023-07-26 16:48:05 +0530

    error fix

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from "react-redux";
 import { store, persistor } from "./redux/store";
 import { PersistGate } from 'redux-persist/integration/react'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
       <BrowserRouter>
@@ -15,4 +14,4 @@ root.render(
       </BrowserRouter>
     </PersistGate>
   </Provider>
-);
+  , document.getElementById('root'));
